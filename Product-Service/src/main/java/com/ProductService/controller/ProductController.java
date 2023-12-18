fb6a641c9914ebcf89d80a0a133409e3fa1cf75a
Abdullah Anli <anliabdullah08@gmail.com>
2023-12-18 17:25:29 +0300

    Product Controller servisi ile mapping işlemleri yapıldı

--- Product-Service/src/main/java/com/ProductService/controller/ProductController.java
+++ Product-Service/src/main/java/com/ProductService/controller/ProductController.java
@@ -1,11 +1,46 @@
 package com.ProductService.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ProductService.entity.Product;
+import com.ProductService.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+
+    }
+
+    @GetMapping("/{productId}")
+    public Product getProdctById(@PathVariable Long productId){
+        return productService.getProductById(productId);
+    }
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
+
+
+    @PutMapping("/{productId}")
+    protected Product updateProduct(@PathVariable Long productId ,
+                                    @RequestBody Product updatedProduct){
+        return productService.updateProduct(productId,updatedProduct);
+
+    }
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long productId){
+        productService.deleteProduct(productId);
+        return ResponseEntity.ok().build();
+
+    }
 
 
 }
